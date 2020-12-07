04ab7ee621e384d7bc70ed30acd7a9b5180018db
Kevin Fu <kevin.fu808@gmail.com>
2020-12-06 18:20:23 -0800

    Migrating the HomePage contents in the App.tsx into it's own Page component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
 
 test("node environment is test", () => {
   expect(process.env.NODE_ENV).toEqual("test");
-});
-
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import logo from "./assets/logo.svg";
-import "./App.scss";
-
 import ServerStatus from "./components/ServerStatus";
+import HomePage from "./pages/HomePage";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/status" component={ServerStatus}/>
+          <Route exact path="/status" component={ServerStatus} />
           {
             // No patch to match everything other than status
           }
-          <Route>
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
-          </Route>
+          <Route component={HomePage} />
         </Switch>
       </Router>
     );
